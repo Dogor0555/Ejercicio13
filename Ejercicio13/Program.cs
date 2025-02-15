b9f775d82e0be5255072b7bbac8d9faeff58e19f
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:17:03 -0600

    Ejercicio13

--- Ejercicio13/Program.cs
+++ Ejercicio13/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
